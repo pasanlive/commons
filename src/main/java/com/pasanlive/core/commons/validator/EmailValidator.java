76e6811abae437d5e5729028c173ea12522737f1
Pasan Buddhika Weerathunga <me@pasanlive.com>
2016-08-01 19:52:24 +0530

    Email validator added with test

--- src/main/java/com/pasanlive/core/commons/validator/EmailValidator.java
+++ src/main/java/com/pasanlive/core/commons/validator/EmailValidator.java
@@ -7,6 +7,11 @@ package com.pasanlive.core.commons.validator;
 public class EmailValidator implements Validator {
     @Override
     public ValidationResult validate(String email) {
-        return ValidationResult.createValidResult();
+//        String EMAIL_REGEX = "^[\\w_].[\\w-_\\.+][^..]*[\\w-_\\.][\\w-_]+@([\\w-]+\\.)+[\\w]+[\\w]$";
+        String EMAIL_REGEX = "^[\\w_].[+\\w\\.\\-']+[^..]*[\\w-_]+@[a-zA-Z0-9-]+(\\.([A-Za-z]{2,})){1,2}+$";
+
+        if(email.matches(EMAIL_REGEX))
+            return ValidationResult.createValidResult();
+        return ValidationResult.createInvalidResult("Invalid email", new ValidationFailException());
     }
 }
--- src/test/java/com/pasanlive/core/commons/validator/EmailValidatorTest.java
+++ src/test/java/com/pasanlive/core/commons/validator/EmailValidatorTest.java
@@ -2,13 +2,40 @@ package com.pasanlive.core.commons.validator;
 
 import junit.framework.TestCase;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class EmailValidatorTest extends TestCase {
 
     public void testValidate() throws Exception {
-        EmailValidator emailValidator = new EmailValidator();
-        ValidationResult result = emailValidator.validate("me@pasanlive.com");
+        ClassLoader classLoader = getClass().getClassLoader();
+        File validEmailsFile = new File(classLoader.getResource("valid_emails").getFile());
+        File invalidEmailsFile = new File(classLoader.getResource("invalid_emails").getFile());
 
-        assertNotNull("Validation result cannot be null", result);
-        assertTrue("Validation should be success", result.isValid());
+        Scanner validEmailsFileScanner = new Scanner(validEmailsFile);
+
+        while (validEmailsFileScanner.hasNext()) {
+            String email = validEmailsFileScanner.next();
+            EmailValidator emailValidator = new EmailValidator();
+            ValidationResult result = emailValidator.validate(email);
+
+            assertNotNull("Validation result cannot be null", result);
+            assertTrue("[" + email + "]  should be success", result.isValid());
+        }
+
+        Scanner invalidEmailFileScanner = new Scanner(invalidEmailsFile);
+
+        while (invalidEmailFileScanner.hasNext()) {
+            String email = invalidEmailFileScanner.next();
+            EmailValidator emailValidator = new EmailValidator();
+            ValidationResult result = emailValidator.validate(email);
+
+            assertNotNull("Validation result cannot be null", result);
+            assertTrue("[" + email + "] should be invalid", !result.isValid());
+            assertNotNull("Description should me available", result.getDescription());
+            assertNotNull("Exception cannot be null", result.getValidationFailException());
+        }
+
+
     }
 }
